6fa22a7d1e5bb2588ce401fa8fd1265a2959ea3f
idobalul <89575272+idobalul@users.noreply.github.com>
2021-12-29 18:31:20 +0200

    users can send messages

--- back/index.js
+++ back/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const { getUsers, removeUser } = require('./controllers/main');
 
 const loginRouter = require('./routers/login');
 
@@ -12,6 +15,24 @@ app.use(cors());
 
 app.use('/login', loginRouter);
 
-app.listen(port, () => {
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+	console.log(socket.id);
+	io.emit('getUsers', getUsers());
+
+	socket.on('sendMessage', (message) => {
+		socket.broadcast.emit('receiveMessage', message);
+	});
+
+	socket.on('disconnect', () => {
+		const { name } = socket.handshake.query;
+		removeUser(name);
+		io.emit('getUsers', getUsers());
+	});
+});
+
+httpServer.listen(port, () => {
 	console.log(`server runs on port: ${port}`);
 });
--- front/src/components/Chat.jsx
+++ front/src/components/Chat.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { io } from 'socket.io-client';
 import { useLocation } from 'react-router-dom';
 import { Stack, InputGroup, FormControl, Button } from 'react-bootstrap';
 
@@ -8,22 +9,53 @@ import Message from './Message';
 export default function Chat() {
 	const location = useLocation();
 	const { username } = location.state;
+	const input = useRef();
+	const socket = useRef();
+	const [messages, setMessages] = useState([]);
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		socket.current = io('http://localhost:8080', {
+			transports: ['websocket'],
+			query: { name: username },
+		});
+		socket.current.on('connect', () => {
+			socket.current.on('getUsers', (USERS) => {
+				setUsers(USERS);
+			});
+		});
+		socket.current.on('receiveMessage', (message) => {
+			setMessages((allMessages) => [...allMessages, message]);
+		});
+	}, []);
+
+	const sendMessage = () => {
+		if (input.current.value === '') {
+			return;
+		}
+		socket.current.emit('sendMessage', input.current.value);
+		setMessages([...messages, input.current.value]);
+	};
 
 	return (
 		<div id="chat-page">
 			<div className="left">
 				<h3>Users</h3>
 				<Stack gap={2}>
-					<User user={username} />
+					{users.map((user) => (
+						<User key={user} user={user} />
+					))}
 				</Stack>
 			</div>
 			<div className="right">
 				<div className="messages">
-					<Message message="hello world" />
+					{messages.map((msg) => (
+						<Message message={msg} />
+					))}
 				</div>
 				<InputGroup className="mb-3" size="lg">
-					<FormControl placeholder="Write you message here" />
-					<Button variant="outline-primary" id="button-addon2">
+					<FormControl ref={input} placeholder="Write you message here" />
+					<Button variant="outline-primary" id="button-addon" onClick={sendMessage}>
 						Send
 					</Button>
 				</InputGroup>
