a2e3f5498a296e695faf0461b84f621f07854666
idobalul <89575272+idobalul@users.noreply.github.com>
2022-02-22 16:30:08 +0200

    Added comments

--- back/controllers/main.js
+++ back/controllers/main.js
@@ -1,41 +1,51 @@
+// An object that holds the username and the socket of the user.
 const USERS = {};
 
+// login handles the login process of a user.
 exports.login = (req, res) => {
+	// Gets the username from the request body.
 	const { username } = req.body;
 
+	// If the username is shorter then 2 characters return status 400 (bad request).
 	if (username.length < 2) {
 		res.status(400).send({ message: 'username must be at least 2 characters long' });
 		return;
 	}
 
+	// If the username is already taken return status 409 (conflict).
 	const isExist = Object.prototype.hasOwnProperty.call(USERS, username);
-
 	if (isExist) {
 		res.status(409).send({ message: `${username} is already used, choose a different name` });
 		return;
 	}
 
+	// If everything is ok initiate the key fot the user in the USERS object and return status 200 (ok).
 	USERS[username] = undefined;
 
 	res.status(201).send({ message: `welcome ${username}!` });
 };
 
+// assignSocket assigns the socket to the user.
 exports.assignSocket = (name, socket) => {
 	USERS[name] = socket;
 };
 
+// getUsers returns an array of all the users.
 exports.getUsers = () => {
 	const users = Object.keys(USERS);
 	return users;
 };
 
+// removeUser removes the user from the USERS object.
 exports.removeUser = (name) => {
 	delete USERS[name];
 };
 
+// findId returns the id of the user or undefined if the user does not exist.
 exports.findId = (name) => {
 	const user = USERS.find((usr) => usr.name === name);
 	return user.id;
 };
 
+// getUserSocket returns the socket of the user or undefined if the user does not exist.
 exports.getUserSocket = (name) => USERS[name] || undefined;
--- back/index.js
+++ back/index.js
@@ -24,20 +24,29 @@ app.get('/', (req, res) => {
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+// Handles the socket connection.
 io.on('connection', (socket) => {
 	console.log(socket.id);
+
+	// Gets the name of the user from the socket and assign the socket of the user in the USERS (see controllers/main.js) object.
 	const { name } = socket.handshake.query;
 	assignSocket(name, socket);
 
+	// When the user is connected update the users list with the new user
 	io.emit('getUsers', getUsers());
+	// Broadcast to all users that the new user joined the chat.
 	socket.broadcast.emit('receiveMessage', `${name} has joined the chat`);
 
+	// Handles the send message event.
 	socket.on('sendMessage', (message) => {
 		io.emit('receiveMessage', `${name}: ${message}`);
 	});
 
+	// Handles the private message event.
 	socket.on('private', (data) => {
+		// Gets the message and the receiver of the private message.
 		const { to, message } = data;
+		// Gets the socket of the receiver.
 		const user = getUserSocket(to);
 
 		// if for some reason the user that should receive the message disconnected the sender gets a message
@@ -62,9 +71,15 @@ io.on('connection', (socket) => {
 		user.emit('receivePrivate', { from: name, message });
 	});
 
+	// Handles the disconnect event.
 	socket.on('disconnect', () => {
+		// Removes the user from the USERS (see controllers/main.js) object.
 		removeUser(name);
+
+		// Updates the users list with disconnected user removed.
 		io.emit('getUsers', getUsers());
+
+		// Broadcast to all users that the user disconnected.
 		socket.broadcast.emit('receiveMessage', `${name} has left the chat`);
 	});
 });
